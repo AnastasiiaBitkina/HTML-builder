11cf45a8263b3c15bbeaa110c51b5fcec247aa1f
anastasia <anastasia.ethno@gmail.com>
2023-05-03 11:29:26 +0200

    fix: added 05-merge-styles solution without sync

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,17 +1,36 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
+
 // выделем путь к стилям
 const stylesDir = path.join(__dirname, 'styles');
 // выделяем путь к файлу к стилям в папке project-dist
 const bundleFilePath = path.join(__dirname, 'project-dist', 'bundle.css');
-const stylesFiles = fs.readdirSync(stylesDir);
-// создаем массив для стилей
-const stylesArray = [];
-// проверяем, что это файл и имеет расширение стилей и добавалем его содержимое в массив со стилями
-stylesFiles.forEach(file => {
-  if (fs.statSync(path.join(stylesDir, file)).isFile() && path.extname(file) === '.css') {
-    stylesArray.push(fs.readFileSync(path.join(stylesDir, file), 'utf8'));
-  }
-});
-// записываем стили в файл
-fs.writeFileSync(bundleFilePath, stylesArray.join('\n'));
+// создаем массив для стилей, проверяем, что это файл и имеет расширение стилей и добавалем его содержимое в массив со стилями
+fs.readdir(stylesDir)
+  .then((stylesFiles) => {
+  
+    const stylesArray = [];
+
+    const filePromises = stylesFiles.map((file) => {
+      
+      const filePath = path.join(stylesDir, file);
+      return fs.stat(filePath)
+        .then((stat) => {
+          if (stat.isFile() && path.extname(file) === '.css') {
+            return fs.readFile(filePath, 'utf8')
+              .then((data) => {
+                stylesArray.push(data);
+              });
+          }
+        });
+    });
+
+    return Promise.all(filePromises)
+      .then(() => {
+        // записываем все стили в выходной файл bundle.css
+        return fs.writeFile(bundleFilePath, stylesArray.join('\n'));
+      });
+  })
+  .catch((err) => {
+    console.error(err);
+  });
